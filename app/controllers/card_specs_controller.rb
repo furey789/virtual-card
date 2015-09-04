5ce59e11ee7fa3d568c17af1e6df38eab9dda449
Peter Furey <prfurey@gmail.com>
2015-09-04 08:06:01 -0600

    Add access to persons profile via token

--- app/controllers/card_specs_controller.rb
+++ app/controllers/card_specs_controller.rb
@@ -2,7 +2,11 @@
 class CardSpecsController < ApplicationController
 
   def index
-
+    oauth = LinkedIn::OAuth2.new
+    # code = "THE_OAUTH_CODE_LINKEDIN_GAVE_ME"
+    access_token = oauth.get_access_token(code)
+    api = LinkedIn::API.new(access_token)
+    @my_name = api.profile(fields: ["first-name", "last-name"])
   end
-  
+
 end
