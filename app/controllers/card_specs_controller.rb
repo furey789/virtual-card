40add5e6c25aa1e845fed7cde54c32912a9b5f93
Peter Furey <prfurey@gmail.com>
2015-09-04 09:15:39 -0600

    Work in progress

--- app/controllers/card_specs_controller.rb
+++ app/controllers/card_specs_controller.rb
@@ -6,8 +6,9 @@ class CardSpecsController < ApplicationController
     # code = "THE_OAUTH_CODE_LINKEDIN_GAVE_ME"
     url_str = request.original_url
     start_id = url_str.index('code=')+5
-    end_id = url_str.length
-    code = url_str.slice(start_id,end_id)
+    end_id = url_str.index('&state=')
+    length = end_id-start_id
+    code = url_str.slice(start_id,length)
 
     oauth = LinkedIn::OAuth2.new
     access_token = oauth.get_access_token(code)
