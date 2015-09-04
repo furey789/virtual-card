68e19461d11bd0614afde672480770b3ca618134
Peter Furey <prfurey@gmail.com>
2015-09-04 08:29:54 -0600

    Work in progress

--- app/controllers/card_specs_controller.rb
+++ app/controllers/card_specs_controller.rb
@@ -2,11 +2,12 @@
 class CardSpecsController < ApplicationController
 
   def index
-    oauth = LinkedIn::OAuth2.new
+    @my_name = request.original_url
+    # oauth = LinkedIn::OAuth2.new
     # code = "THE_OAUTH_CODE_LINKEDIN_GAVE_ME"
-    access_token = oauth.get_access_token(code)
-    api = LinkedIn::API.new(access_token)
-    @my_name = api.profile(fields: ["first-name", "last-name"])
+    # access_token = oauth.get_access_token(code)
+    # api = LinkedIn::API.new(access_token)
+    # @my_name = api.profile(fields: ["first-name", "last-name"])
   end
 
 end
