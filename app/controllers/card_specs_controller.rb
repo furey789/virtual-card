f4d20255fcf1efcc93f5718af532cd27d3036b47
Peter Furey <prfurey@gmail.com>
2015-09-05 09:20:10 -0600

    Work in progress

--- app/controllers/card_specs_controller.rb
+++ app/controllers/card_specs_controller.rb
@@ -14,16 +14,24 @@ class CardSpecsController < ApplicationController
     access_token = oauth.get_access_token(code)
     api = LinkedIn::API.new(access_token)
     person = api.profile(fields: ["first-name", "last-name", "headline", "location", "picture-url", "email-address", "public-profile-url"])
-    @person = person.to_hash  # first-name becomes first_name, etc.
+    person = person.to_hash  # first-name becomes first_name, etc.
 
-    person_in_db = "no"
+    country =  person["location"]["country"]["code"].upcase
+    if country == 'US'
+      country = country +'A'
+    end
+    location = person["location"]["name"] + ", " + country
+
+    @person_user = "no"
     User.all.each do |user|
-      if user["email_address"] == @person["email_address"]
-        person_in_db = "yes"
+      if user["first_name"] == person["first_name"] and user["last_name"] == person["last_name"] and user["email_address"] == person["email_address"]
+        @person_user = user
       end
     end
-    if person_in_db == "no"
-      User.create("first_name" => @person["first_name"], "last_name" => @person["last_name"], "headline" => @person["headline"], "location" => @person["location"], "picture_url" => @person["picture_url"], "email_address" => @person["email_address"], "public_profile_url" => @person["public_profile_url"] )
+
+    if @person_user == "no"
+      User.create("first_name" => person["first_name"], "last_name" => person["last_name"], "headline" => person["headline"], "location" => location, "picture_url" => person["picture_url"], "email_address" => person["email_address"], "public_profile_url" => person["public_profile_url"] )
+      @person_user = User.last
     end
 
   end
