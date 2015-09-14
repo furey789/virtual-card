00bb0e25962a19090d4ed3de3e4eb86e121c4709
Peter Furey <prfurey@gmail.com>
2015-09-14 14:37:29 -0600

    Remove one hardcoded url

--- app/controllers/card_mailer_controller.rb
+++ app/controllers/card_mailer_controller.rb
@@ -4,8 +4,6 @@ class CardMailerController < ApplicationController
   def create
 
     @person_user = User.find(session[:user_id])
-    # @url = 'http://localhost:3000/card_specs/'+@person_user.id.to_s
-    # @url = 'https://virtual-card.herokuapp.com/card_specs/'+@person_user.id.to_s
     path_url = request.original_url
     @url = path_url.split('card_mailer')[0]+'card_specs/'+@person_user.id.to_s
 
