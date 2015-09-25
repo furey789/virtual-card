a5aa2e18a49d9bc45d941ea9f43b92bd36ff02d3
Peter Furey <prfurey@gmail.com>
2015-09-25 09:35:39 -0600

    Work in progress

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -32,6 +32,7 @@ Rails.application.configure do
   #config.assets.compile = false
 
   # Make this true for Heroku
+  config.serve_static_files = true
   config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
